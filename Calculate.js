609737bb778b9bede64faf9815033a4c288f7780
ArlenChu <arlensmchu@gmail.com>
2026-07-18 16:04:17 +0800

    add math function'

---
+++ Calculate.js
@@ -0,0 +1,15 @@
+function add(oprand1, oprand2) {
+    return oprand1 + oprand2
+}
+
+function subtract(oprand1, oprand2) {
+    return oprand1 - oprand2
+}
+
+function multiply(oprand1, oprand2) {
+    return oprand1 * oprand2
+}
+
+function divide(oprand1, oprand2) {
+    return oprand1 / oprand2
+}
