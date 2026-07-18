ee33738032e6920c93064bf236120c1b26ba35e7
ArlenChu <arlensmchu@gmail.com>
2026-07-18 16:11:13 +0800

    add operate fucntion

--- Calculate.js
+++ Calculate.js
@@ -1,15 +1,31 @@
 function add(oprand1, oprand2) {
-    return oprand1 + oprand2
+    return oprand1 + oprand2;
 }
 
 function subtract(oprand1, oprand2) {
-    return oprand1 - oprand2
+    return oprand1 - oprand2;
 }
 
 function multiply(oprand1, oprand2) {
-    return oprand1 * oprand2
+    return oprand1 * oprand2;
 }
 
 function divide(oprand1, oprand2) {
-    return oprand1 / oprand2
+    return oprand1 / oprand2;
+}
+
+let oprand1;
+let oprand2;
+let operator;
+
+function operate(oprand1, oprand2, operator) {
+    if (operator == "+") {
+        return add(oprand1, oprand2)
+    } else if (operator == "-") {
+        return subtract(oprand1, oprand2)
+    } else if (operator == "*") {
+        return mulitply(oprand1, oprand2)
+    } else if (operator == "/") {
+        return divide(oprand1, oprand2)
+    }
 }
